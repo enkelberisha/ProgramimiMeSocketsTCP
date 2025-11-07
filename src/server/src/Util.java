24517f44c018fd81dcd82a5914f278e2ec84613f
vleragalica <vleragalica@gmail.com>
2025-11-07 04:38:14 +0100

    Created Util.java

---
+++ src/server/src/Util.java
@@ -0,0 +1,20 @@
+package server.src;
+
+import java.io.*;
+
+
+public class Util {
+    public static void writeln(BufferedWriter out, String s) throws IOException {
+        out.write(s);
+        out.write("\n");
+        out.flush();
+    }
+    public static void replyOk(BufferedWriter out, String body) throws IOException {
+        writeln(out, "OK " + (body == null ? "" : body));
+        writeln(out, ".");
+    }
+    public static void replyErr(BufferedWriter out, String body) throws IOException {
+        writeln(out, "ERR " + (body == null ? "" : body));
+        writeln(out, ".");
+    }
+}
